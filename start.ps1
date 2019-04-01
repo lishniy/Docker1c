2603318410dce04548bbcddb653f9baf5c79718e
Bezeus <lordbezeus80@gmail.com>
2019-04-01 13:36:05 +0300

    Add files via upload
    Версию захардкодил 8.3.14.1630. И убрал в связи с этим командлет Get-WmiObject
    Пути поменял с учётом x64 версии
    Имя службы тоже откорректировано

--- start.ps1
+++ start.ps1
@@ -3,26 +3,44 @@ param(
 [int]$port=1540,
 [string]$range="1560:1591",
 [string]$debug="N",
-[string]$servpath="C:\Program Files (x86)\1cv8\srvinfo"
+[string]$servpath="C:\Program Files\1cv8\srvinfo",
+[string]$log="N",
+[string]$version="8.3.14.1630"
 )
 #range - для рабочего процесса;
 #regport - для менеджера кластера;
 #port - для агента сервера (не обязательно, если центральный сервер кластера один).
-$version=Get-WmiObject -Class Win32_Product | where {$_.name -like '*1C*'} | select -ExpandProperty "version"
-$1cpath="C:\Program Files (x86)\1cv8\$version\bin"
-Set-MpPreference -DisableRealtimeMonitoring $true
-$name="""$1cpath\ragent.exe"" -srvc -agent -regport $regport -port $port -range $range -d ""$servpath"""
+
+#$ipaddress=Get-NetIPAddress -SuffixOrigin "Manual" | Select-Object -ExpandProperty "IPAddress"
+#write-host "Server 1C ip: " $ipaddress
+Get-NetIPAddress | Format-Table
+
+write-host "Installed version: " ($version)
+
+$1cpath="C:\Program Files\1cv8\$version\bin"
+$name="""$1cpath\ragent.exe"" -srvc -agent -regport $regport -port $port -range $range -d "
 if($debug -eq "Y")
 {
+    write-host "Debug: enabled"
     $name=$name + " -debug"
 }
-write-host ($version)
-Write-Verbose "Stoping 1C service"
-stop-service '1C:Enterprise 8.3 Server Agent'
-Write-Verbose "Setting up 1C service"
-set-itemproperty -path 'HKLM:\SYSTEM\CurrentControlSet\Services\1C:Enterprise 8.3 Server Agent' -name ImagePath -value $name ;
-Write-Verbose "Starting 1C service"
-start-service '1C:Enterprise 8.3 Server Agent'
-Write-Verbose "1C service started"
+$name=$name + """$servpath"""
+write-host "Service ready to stop"
+Stop-Service "1C:Enterprise 8.3 Server Agent (x86-64)"
+write-host "Service was stoped"
+if($log -eq "Y")
+{
+    write-host "Log path: c:\srvinfo\log\"
+}
 
-c:\Wait-Service.ps1 -ServiceName '1C:Enterprise 8.3 Server Agent' -AllowServiceRestart
+# if($repair -eq "Y")
+# {
+#     write-host "Repaire platform"
+#     Copy-Item c:\backbas.dll $1cpath -force
+# }
+
+set-itemproperty -path 'HKLM:\SYSTEM\CurrentControlSet\Services\1C:Enterprise 8.3 Server Agent (x86-64)' -name ImagePath -value $name ;
+#Run depend services
+#get-service '1C:Enterprise 8.3 Server Agent' | select-object -expand ServicesDependedOn | Where-Object status -eq "Stopped" | Start-Service
+Start-Service '1C:Enterprise 8.3 Server Agent (x86-64)'
+c:\Wait-Service.ps1 -ServiceName '1C:Enterprise 8.3 Server Agent (x86-64)' -AllowServiceRestart
